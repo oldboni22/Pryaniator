777cdf5686d902d911d11b896b348019157dc22c
oldboni22 <oldboni22@gmail.com>
2026-02-18 12:40:23 +0300

    small mediator change

--- Pryaniator/Mediator.cs
+++ Pryaniator/Mediator.cs
@@ -10,9 +10,9 @@ public sealed class Mediator(
     public async Task<IEnumerable<object?>> SendAsync<TSignal>(TSignal signal, CancellationToken cancellationToken = default) 
         where TSignal : Signal
     {
-        var signalHandlers = handlers[typeof(TSignal)].ToList();
+        var signalHandlers = handlers[typeof(TSignal)];
         
-        var results = new List<object?>(signalHandlers.Count);
+        var results = new List<object?>();
 
         foreach (var handler in signalHandlers)
         {
